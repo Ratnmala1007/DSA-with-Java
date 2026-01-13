58280891dcc3b8bfb95dd4a610146c231d87ac54
Ratnmala1007 <ratnamalas1007@gmail.com>
2026-01-14 03:33:24 +0530

    created pattern program using for loop

--- CodingBlocks_DSAwithJava/src/com/dsa/day02/Pattern_6.java
+++ CodingBlocks_DSAwithJava/src/com/dsa/day02/Pattern_6.java
@@ -7,8 +7,8 @@ package com.dsa.day02;
 public class Pattern_6 {
     public static void main(String[] args) {
         int n = 5;
-        int row = 0;
-        for (row = 0; row < n; row++) {
+        int row = 1;
+        for (row = 1; row <=n; row++) {
 
             for (int space = 0; space < n - row; space++) {
 
